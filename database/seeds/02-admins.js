a2cd9bc5cc001c7ff14041c092ea5e89bb826443
DentonWarnock <dentonwarnock@gmail.com>
2020-02-27 19:55:09 -0800

    updated seed files

--- database/seeds/02-admins.js
+++ database/seeds/02-admins.js
@@ -1,8 +1,11 @@
 exports.seed = function(knex) {
-  return knex("admin").insert([
-    {
-      id: 1,
-      user_id: 1
-    }
-  ]);
+  return knex("admin")
+    .del()
+    .then(function() {
+      return knex("admin").insert([
+        {
+          user_id: 1
+        }
+      ]);
+    });
 };
--- database/seeds/05-todos.js
+++ database/seeds/05-todos.js
@@ -1,28 +1,29 @@
 exports.seed = function(knex) {
-  return knex("todos").insert([
-    {
-      id: 1,
-      title: "Create Database Schema",
-      description: "Write down tables, columns, and foreign keys",
-      is_completed: true,
-      admin_id: 1,
-      volunteer_id: 2
-    },
-    {
-      id: 2,
-      title: "Build Back End API",
-      description: "Build Week Unit 4 - School in the Cloud",
-      is_completed: false,
-      admin_id: 1,
-      volunteer_id: 2
-    },
-    {
-      id: 3,
-      title: "Deploy and Document API",
-      description: "Use Heroku to deploy and Postman for documentation",
-      is_completed: false,
-      admin_id: 1,
-      volunteer_id: 2
-    }
-  ]);
+  return knex("todos")
+    .del()
+    .then(function() {
+      return knex("todos").insert([
+        {
+          title: "Create Database Schema",
+          description: "Write down tables, columns, and foreign keys",
+          is_completed: true,
+          admin_id: 1,
+          volunteer_id: 2
+        },
+        {
+          title: "Build Back End API",
+          description: "Build Week Unit 4 - School in the Cloud",
+          is_completed: false,
+          admin_id: 1,
+          volunteer_id: 2
+        },
+        {
+          title: "Deploy and Document API",
+          description: "Use Heroku to deploy and Postman for documentation",
+          is_completed: false,
+          admin_id: 1,
+          volunteer_id: 2
+        }
+      ]);
+    });
 };
