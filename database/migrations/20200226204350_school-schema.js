cfadea93c22e76d9fcee5d2661d890eedda41503
DentonWarnock <dentonwarnock@gmail.com>
2020-02-27 18:24:54 -0800

    changed schema to allow longer strings for password.

--- database/migrations/20200226204350_school-schema.js
+++ database/migrations/20200226204350_school-schema.js
@@ -6,9 +6,9 @@ exports.up = function(knex) {
         .string("email")
         .notNullable()
         .unique();
-      users.string("password", 48).notNullable();
-      users.string("first_name", 48).notNullable();
-      users.string("last_name", 48).notNullable();
+      users.string("password").notNullable();
+      users.string("first_name").notNullable();
+      users.string("last_name").notNullable();
       users.string("role", 16).notNullable();
     })
     .createTable("admin", admins => {
