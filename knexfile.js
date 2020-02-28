cf01d97474ce71ba5755b41dddd1c906bc30e385
DentonWarnock <dentonwarnock@gmail.com>
2020-02-27 17:33:27 -0800

    fixed knex file added PG database

--- knexfile.js
+++ knexfile.js
@@ -23,5 +23,14 @@ module.exports = {
       tableName: "dbmigrations"
     },
     seeds: { directory: "./database/seeds" }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: { directory: "./database/seeds" }
   }
 };
